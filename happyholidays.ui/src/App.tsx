5969268d44f2c3cf10718cb5dce3d2b113ff659c
Bazil Malekar <bazil@datyasolutions.com>
2024-08-02 18:40:53 +0530

    added navbar, configured redux-toolkit

--- happyholidays.ui/src/App.tsx
+++ happyholidays.ui/src/App.tsx
@@ -2,12 +2,14 @@ import "./App.css";
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import Topbar from "./components/Topbar/Topbar";
+import Header from "./components/Header/Header";
 
 const App = () => {
   return (
     <>
     <ToastContainer />
     <Topbar />
+    <Header />
     Awesome!
     </>
   )
