9f51c96bb5d4b398189265fa9ae9402a32aaa56b
Bazil Malekar <bazil@datyasolutions.com>
2024-08-04 12:08:46 +0530

    Added home components

--- happyholidays.ui/src/App.tsx
+++ happyholidays.ui/src/App.tsx
@@ -4,16 +4,16 @@ import 'react-toastify/dist/ReactToastify.css';
 import Topbar from "./components/Topbar/Topbar";
 import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
-import { Routes, Route } from "react-router-dom";
 import Home from "./components/Home/Home";
 import International from "./components/Home/Hero/International";
 import Domestic from "./components/Home/Hero/Domestic/Domestic";
 import AboutUs from "./components/Home/AboutUs/AboutUs";
+import { Routes, Route } from "react-router-dom";
 
 const App = () => {
   return (
     <>
-      <ToastContainer />
+    <ToastContainer />
       <Topbar />
       <Header />
       <Routes>
